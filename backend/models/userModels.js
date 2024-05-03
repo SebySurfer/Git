f685390e2ad4625ab88e37cd9007b0f7d08aff98
SebySurfer <sebysurfer@gmail.com>
2024-05-03 10:35:16 -0600

    Modified userModels

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,5 +1,6 @@
-const { Timestamp } = require('mongodb')
-const mongoose = require('mongoose')
+const mongoose = require('mongodb');
+const Schema = mongoose.Schema;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -11,21 +12,17 @@ const userSchema = mongoose.Schema({
         required: [true, "Por favor teclea tu email"],
         unique: true
     },
-    password: {
-        type: String,
-        required: [true, "Por favor teclea tu password"]
-    },
-    esAdmin: {
-        type: Boolean,
-        default: false
+    phoneNumber: {
+        type: Int32Array,
+        required: [true, "Por favor teclea tu numero"]
     }
 
+});
 
 
-}, {
-    timestamps: true
-}
+const User = mongoose.model('User', userSchema );
 
-) 
+module.exports = {
+    User
+};
 
-module.exports = mongoose.model('User', userSchema)
