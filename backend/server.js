41448ef89fe75148aaad5f95157096626c7e7693
SebySurfer <sebysurfer@gmail.com>
2024-05-03 19:40:23 -0600

    Link Corrected (by teacher)

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,31 @@
-console.log("hola mundo")
+
 
 const express = require('express')
+const colors = require('colors')
 const connectDB = require('./config/db')
-// Connect to MongoDB
-connectDB();
-
-console.log('MongoDB URL:', process.env.MONGO_URL);
-
-
+const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 
 
-const dotenv = require('dotenv').config()
-
-
-console.log('Loaded environment variables:', process.env);
-
-
+// Connect to MongoDB
+connectDB();
 
 const port = process.env.PORT || 3000
-//const mongourl = process.env.MONGO_URL;
+
+console.log('MongoDB URL:', process.env.MONGO_URL);
 
 const app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
-
 
 // Routes
 app.use('/api/tareas', require('./routes/tareasRoutes'))
 
 // Error handling middleware
-//const { errorHandler } = require('./middleware/errorMiddleware');
 app.use(errorHandler);
 
 // Start server
 app.listen(port, ()=> console.log(`Servidor iniciado en el puerto ${port}`))
+
+
+console.log('Loaded environment variables:', process.env);
